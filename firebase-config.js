4973963f7f6168f7f60867f8951c73ea405b5650
msprk <msprk@users.noreply.github.com>
2026-05-14 23:34:04 +0200

    Pridanie zdielaneho firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyDOYPIXjbW3mrFpospLjTbt6ui4I318sg8",
+  authDomain:        "msprk-app.firebaseapp.com",
+  databaseURL:       "https://msprk-app-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId:         "msprk-app",
+  storageBucket:     "msprk-app.firebasestorage.app",
+  messagingSenderId: "544350754193",
+  appId:             "1:544350754193:web:acb050a43126437790f630"
+};
